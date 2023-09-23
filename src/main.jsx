1b6baa66aa3913cdb4b2b09703eebbdc3cf99066
Amimu10 <amimul211@gmail.com>
2023-09-23 13:38:11 +0600

    change path

--- src/main.jsx
+++ src/main.jsx
@@ -32,12 +32,12 @@ const router = createBrowserRouter([
       { 
         path: "/job/:id", 
         element: <JobDetails></JobDetails>,
-        loader: () => fetch("../public/jobs.json") 
+        loader: () => fetch("/jobs.json")  
       },
       {
         path: "/applied/",
         element: <AppliedJobs></AppliedJobs>,
-        loader: () => fetch("../public/jobs.json") 
+        loader: () => fetch("/jobs.json") 
       }
     ]
   },
